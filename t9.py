55b2db0ef671f200cf574c0bf2c78cc90a72b997
AliensWithdrawPeople <daniilivanov1606@gmail.com>
2022-11-21 14:46:48 +0700

    t9 final

--- t9.py
+++ t9.py
@@ -2,7 +2,6 @@ import numpy as np
 import numpy.typing as npt
 import matplotlib.pyplot as plt
 
-
 def tridiagMatrixAlg(L1: float, L2: float, N: int)->npt.ArrayLike:
     h = np.fabs(L1 - L2) / N
     x = np.linspace(L1, L2, N)
@@ -36,16 +35,10 @@ def tridiagMatrixAlg(L1: float, L2: float, N: int)->npt.ArrayLike:
         i = i - 1
     return ys
 
-L1 = -np.pi / 2
-L2 = np.pi / 2
-x = np.linspace(L1, L2, 1000)
-ys = tridiagMatrixAlg(L1, L2, 1000)
+boundaries = (-np.pi / 2, np.pi / 2)
+x = np.linspace(*boundaries, 1000)
+ys = tridiagMatrixAlg(*boundaries, 1000)
 yTr = [-np.cos(xx) + 1 * xx + np.pi / 2 for xx in x]
-# yTr = [-np.cos(xx) + 2/np.pi * xx - 1 for xx in x]
-# yTr = [-np.cos(xx) + 3 * xx - 3 * np.pi / 2 for xx in x]
 plt.plot(x, ys, color='blue')
 plt.plot(x, yTr, color='red')
-
-# plt.plot(x[2:], ys[2:], color='blue')
-# plt.plot(x[2:], yTr[2:], color='red')
 plt.show()
